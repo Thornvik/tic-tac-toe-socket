a7280cb402315b44e1a252942f2d55e06ff7c42c
Ture Thörnvik <tthornvik@deloittedigital.se>
2022-04-06 17:14:01 +0200

    feat: user able to join a room

--- src/app.js
+++ src/app.js
@@ -1,6 +1,12 @@
 const express = require("express");
 const http = require("http");
 const socketIo = require("socket.io");
+const {
+  addUser,
+  removeUser,
+  getUser,
+  getUsersInRoom,
+} = require('./users/user')
 
 const port = process.env.PORT || 3001;
 const index = require("./routes/index");
@@ -22,11 +28,32 @@ const io = socketIo(server, {
 io.on("connection", (socket) => {
   console.log("New client connected");
 
+  socket.on('join', ({ username, room }, callback) => {
+    const { error, user } = addUser({ id: socket.id, username, room })
+
+    if (error) {
+      return callback(error)
+    }
+
+    socket.join(user.room)
+
+    io.to(user.room).emit('roomData', {
+      room: user.room,
+      users: getUsersInRoom(user.room)
+    })
+
+    callback()
+  })
+
   socket.on('playerMove', (playingField, cb) => {
-    socket.emit('oponentMove', playingField)
+    const user = getUser(socket.id)
+
+    io.to(user.room).emit('oponentMove', playingField)
   })
   
   socket.on("disconnect", () => {
+    removeUser(socket.id)
+
     console.log("Client disconnected");
   });
 });
