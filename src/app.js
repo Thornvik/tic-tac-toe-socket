0e52b8df8724f2592854ad73d8f15e4eee774036
Ture Thörnvik <tthornvik@deloittedigital.se>
2022-05-19 16:13:50 +0200

    feat:users in a room

--- src/app.js
+++ src/app.js
@@ -18,7 +18,7 @@ const server = http.createServer(app);
 
 const io = socketIo(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: ['http://localhost:3000', 'http://localhost:3002'],
     methods: ["GET", "POST"],
     allowedHeaders: ["my-custom-header"],
     credentials: true
@@ -28,22 +28,32 @@ const io = socketIo(server, {
 io.on("connection", (socket) => {
   console.log("New client connected");
 
-  socket.on('join', ({ username, room }, callback) => {
+  socket.on('join', ({ username, room, players }, callback) => {
     const { error, user } = addUser({ id: socket.id, username, room })
 
     if (error) {
       return callback(error)
     }
 
+    const usersInRoom = getUsersInRoom(user.room)
+
+    if (usersInRoom.length > 1) {
+      players.p1 = usersInRoom[0].username
+      players.p2 = user.username
+    } else {
+      players.p1 = user.username
+    }
+
     socket.join(user.room)
 
     io.to(user.room).emit('roomData', {
       room: user.room,
-      users: getUsersInRoom(user.room)
+      users: getUsersInRoom(user.room),
+      players
     })
   })
 
-  socket.on('playerMove', (playingField, cb) => {
+  socket.on('playerMove', (playingField) => {
     const user = getUser(socket.id)
 
     io.to(user.room).emit('oponentMove', playingField)
